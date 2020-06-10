710f10e0acc32c0c400c77c50cf7f42fc881769c
abigail <abigailboyer1@gmail.com>
2020-06-10 12:52:41 -0500

    create dashboard component to render main content

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Nav from './Nav'
+
+function Dashboard() {
+  return(
+    <main>
+      <Header />
+      <Nav />
+    </main>
+  )
+}
+
+export default Dashboard
