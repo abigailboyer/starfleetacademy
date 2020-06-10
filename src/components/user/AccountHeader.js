85a95ab4670495399c9b7e5c58ffd9ad5918f5df
abigail <abigailboyer1@gmail.com>
2020-06-10 13:05:17 -0500

    create new header component

---
+++ src/components/user/AccountHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AccountHeader() {
+  return (
+    <header id="accountHeader">
+      <h1>Starfleet Academy</h1>
+      <h2>Student Portal</h2>
+    </header>
+  )
+}
+
+export default AccountHeader
