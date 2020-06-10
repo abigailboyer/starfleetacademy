105f95e8ee380dcbc34a0d21a0c1e6bb4d2164b1
abigail <abigailboyer1@gmail.com>
2020-06-10 14:14:32 -0500

    change to class based component

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,30 +1,38 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import AccountHeader from './AccountHeader'
 
-function Login() {
-  return(
-    <main>
-      <AccountHeader />
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-      <section id="signin">
-        <form id="signin-form">
-          <ul>
-            <li>
-              <input type="text" id="username" placeholder="Username" />
-            </li>
-            <li>
-              <input type="password" id="password" placeholder="Password" />
-            </li>
-            <li>
-              <input type="submit" id="signin-form-submit" value="Login" />
-            </li>
-          </ul>
-        </form>
+  render() {
+    return(
+      <main>
+        <AccountHeader />
 
-        <p id="createAccount">Or <a href="/signup">create an account</a></p>
-      </section>
-    </main>
-  )
+        <section id="signin">
+          <form id="signin-form">
+            <ul>
+              <li>
+                <input type="text" id="username" placeholder="Username" />
+              </li>
+              <li>
+                <input type="password" id="password" placeholder="Password" />
+              </li>
+              <li>
+                <input type="submit" id="signin-form-submit" value="Login" />
+              </li>
+            </ul>
+          </form>
+
+          <p id="createAccount">Or <Link to="Signup">create an account</Link></p>
+        </section>
+      </main>
+    )
+  }
 }
 
 export default Login
