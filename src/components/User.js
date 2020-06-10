69012cd9de8463af625956930063a973dcd824f8
abigail <abigailboyer1@gmail.com>
2020-06-09 19:14:44 -0500

    add logout button

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,12 @@ import React from 'react'
 function User() {
   return(
     <section>
-      user
+      {/* 1. icon based upon student's track */}
+      {/* 2. student's name generated from state */}
+      {/* 3. arrow to click on for drop down menu */}
+      {/* drop down menu contains user settings and logout */}
+      James T. Kirk
+      <button id="logout">Logout</button>
     </section>
   )
 }
