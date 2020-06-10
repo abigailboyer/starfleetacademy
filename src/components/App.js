bef84e061c7faed431eab4ad11c2afa00541acde
abigail <abigailboyer1@gmail.com>
2020-06-10 13:33:16 -0500

    add routes to login and signup

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,19 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import '../style.css'
 import Dashboard from './Dashboard'
+import Signup from './user/Signup'
+import Login from './user/Login'
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Dashboard} />
-        </Switch>
-      </Router>
-    </div>
+    /* fine to not include an enclosing tag? */
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Dashboard} />
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
