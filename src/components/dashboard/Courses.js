c960f85d5df8b80d887db8876168afef96de347f
abigail <abigailboyer1@gmail.com>
2020-06-14 11:56:42 -0500

    add heading to courses component

--- src/components/dashboard/Courses.js
+++ src/components/dashboard/Courses.js
@@ -5,6 +5,10 @@ function Courses() {
   /* programatically populate course information */
   return (
     <article className="card" id="courses">
+      <hgroup>
+        <h3>Courses</h3>
+        <h5>Spring 2235</h5>
+      </hgroup>
       <Course />
       <Course />
       <Course />
