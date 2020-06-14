2fd0e9ec5679748251209d8f5518da54ffc9d26a
abigail <abigailboyer1@gmail.com>
2020-06-14 11:12:48 -0500

    hardcode course information

---
+++ src/components/dashboard/Course.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Course() {
+  return(
+    <article id="course">
+      <ul>
+        <li>
+          <h4 class="courseNumber">2035.45291</h4>
+        </li>
+        <li>
+          <h3 class="courseTitle">Warp Theory</h3><p class="courseLevel">PHYS 200</p>
+        </li>
+        <li>
+          <h4 class="instructor">Cdre. Paris</h4>
+        </li>
+        <li>
+          <h4 class="commInstructor">Comm instructor ></h4>
+        </li>
+      </ul>
+    </article>
+  )
+}
+
+export default Course
