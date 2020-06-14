99f45b5893cbe7932c1b9b15cfc512511ead6996
abigail <abigailboyer1@gmail.com>
2020-06-14 11:13:06 -0500

    add hardcoded course components for styling

--- src/components/dashboard/Courses.js
+++ src/components/dashboard/Courses.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Course from './Course'
 
 function Courses() {
+  /* programatically populate course information */
   return (
     <article className="card" id="courses">
-
+      <Course />
+      <Course />
+      <Course />
+      <Course />
+      <Course />
+      <Course />
     </article>
   )
 }
