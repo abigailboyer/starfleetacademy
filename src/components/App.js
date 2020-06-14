60f9e5be1a783172eb705f7de6e6354c6812bc6f
abigail <abigailboyer1@gmail.com>
2020-06-14 01:54:09 -0500

    adjust for new file paths after grouping dashboard components together

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import '../style.css'
-import Dashboard from './Dashboard'
+import Dashboard from './dashboard/Dashboard'
 import Signup from './user/Signup'
 import Login from './user/Login'
 
