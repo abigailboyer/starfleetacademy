2e6c180cc8d01bf49aac998777b47000a925c8fb
abigail <abigailboyer1@gmail.com>
2020-06-14 11:38:22 -0500

    style credit tracker

--- src/components/dashboard/Tracker.js
+++ src/components/dashboard/Tracker.js
@@ -13,7 +13,7 @@ function Tracker() {
           <progress id="water-credits-progress" max="100" value="25">25%</progress>
         </li>
         <li>
-          <label for="energy-progress">Energy Progress</label>
+          <label for="energy-progress">Energy Consumption</label>
           <progress id="energy-progress" max="200" value="10">10%</progress>
         </li>
       </ul>
