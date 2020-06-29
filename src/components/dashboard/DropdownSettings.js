123ba84b6dbb444f1329119411b32f235f2fb89c
abigail <abigailboyer1@gmail.com>
2020-06-29 10:52:04 -0500

    add dropdown settings file

---
+++ src/components/dashboard/DropdownSettings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dropdown from 'react-bootstrap/Dropdown'
+
+function DropdownSettings() {
+  return (
+    <Dropdown>
+      <Dropdown.Item href="/settings">Settings</Dropdown.Item>
+    </Dropdown>
+  )
+}
+
+export default DropdownSettings
