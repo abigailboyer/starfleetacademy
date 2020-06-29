9df1e46afab9f783b76c02c8e080cd485fae2909
abigail <abigailboyer1@gmail.com>
2020-06-29 11:32:05 -0500

    add dropdown menu to nav

--- src/components/dashboard/NavMenu.js
+++ src/components/dashboard/NavMenu.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownSettings from './DropdownSettings'
 
 function NavMenu() {
   return (
     <nav>
-      menu
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Menu
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="/dashboard">Dashboard</Dropdown.Item>
+          <Dropdown.Item href="/comms">Comms</Dropdown.Item>
+          <Dropdown.Item href="/academics">Academics</Dropdown.Item>
+          <Dropdown.Item href="/registration">Registration</Dropdown.Item>
+          <Dropdown.Item href="/library">Library</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="/settings">Settings</Dropdown.Item>
+          <Dropdown.Item href="/">Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </nav>
   )
 }
