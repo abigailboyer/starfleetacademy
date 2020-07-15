d96826da3ada680690662604975c2be21e5efe69
abigail <abigailboyer1@gmail.com>
2020-07-15 16:25:05 -0500

    toggle menu visibility on click

--- src/components/dashboard/NavMenu.js
+++ src/components/dashboard/NavMenu.js
@@ -1,20 +1,45 @@
 import React from 'react'
 
-function NavMenu() {
-  return (
-    <nav>
-      <h5 id="menuTitle">Menu</h5>
-      <ul>
-        <li><a href="/dashboard">Dashboard</a></li>
-        <li><a href="/comms">Comms</a></li>
-        <li><a href="/academics">Academics</a></li>
-        <li><a href="/registration">Registration</a></li>
-        <li><a href="/library">Library</a></li>
-        <li><a href="/settings">Settings</a></li>
-        <li><a href="/">Logout</a></li>
-      </ul>
-    </nav>
-  )
+class NavMenu extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isHidden: true
+    }
+    this.toggleHidden = this.toggleHidden.bind(this)
+  }
+
+  toggleHidden() {
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+    console.log(this.state.isHidden);
+  }
+
+  render () {
+    if(this.state.isHidden) {
+      return(
+        <nav>
+          <button id="menuTitle" onClick={this.toggleHidden}>Menu</button>
+        </nav>
+      )
+    } else {
+      return(
+        <nav>
+          <button id="menuTitle" onClick={this.toggleHidden}>Menu</button>
+          <ul id="menu">
+            <li><a href="/dashboard">Dashboard</a></li>
+            <li><a href="/comms">Comms</a></li>
+            <li><a href="/academics">Academics</a></li>
+            <li><a href="/registration">Registration</a></li>
+            <li><a href="/library">Library</a></li>
+            <li><a href="/settings">Settings</a></li>
+            <li><a href="/">Logout</a></li>
+          </ul>
+        </nav>
+      )
+    }
+  }
 }
 
 export default NavMenu
