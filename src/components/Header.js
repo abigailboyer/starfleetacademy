3b4b0916eeb18768eae4093f6882c5c6b20868e5
abigail <abigailboyer1@gmail.com>
2020-07-15 14:21:47 -0500

    add dropdown menu to desktop screens for settings and user functions

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MediaQuery from 'react-responsive'
 import User from './user/User'
 import NavMenu from './dashboard/NavMenu'
+import DropdownSettings from './dashboard/DropdownSettings'
 
 function Header() {
   return(
@@ -11,7 +12,7 @@ function Header() {
         <NavMenu />
       </MediaQuery>
       <MediaQuery minDeviceWidth={800}>
-        <User />
+        <DropdownSettings />
       </MediaQuery>
     </header>
   );
--- src/components/dashboard/DropdownSettings.js
+++ src/components/dashboard/DropdownSettings.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown'
+import User from '../user/User'
 
 function DropdownSettings() {
   return (
     <Dropdown>
-      <Dropdown.Item href="/settings">Settings</Dropdown.Item>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Menu
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="/settings">Settings</Dropdown.Item>
+        <Dropdown.Item><User /></Dropdown.Item>
+      </Dropdown.Menu>
     </Dropdown>
   )
 }
