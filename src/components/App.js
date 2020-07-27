ac08d2b9f675cd625dec6d003ec90aabdd4dbcee
abigail <abigailboyer1@gmail.com>
2020-07-27 16:30:44 -0500

    switch back to browserrouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import '../style.css'
 import Dashboard from './dashboard/Dashboard'
 import Signup from './user/Signup'
