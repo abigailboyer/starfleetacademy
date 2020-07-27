3902e6e674224bfe1f05323a4b833e3f4a221499
abigail <abigailboyer1@gmail.com>
2020-07-27 10:20:12 -0500

    Updates

---
+++ precache-manifest.f7a4b1b2f038341415d6c415169486c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f493d208b77c6fa948819172e0a13267",
+    "url": "/abigailboyer/starfleetacademy/index.html"
+  },
+  {
+    "revision": "329e447eec8245fecd62",
+    "url": "/abigailboyer/starfleetacademy/static/css/main.c2cfbbc2.chunk.css"
+  },
+  {
+    "revision": "0183bd429da8473d19f5",
+    "url": "/abigailboyer/starfleetacademy/static/js/2.cc277668.chunk.js"
+  },
+  {
+    "revision": "a9d1a04980daa70b123310242d9dcc58",
+    "url": "/abigailboyer/starfleetacademy/static/js/2.cc277668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329e447eec8245fecd62",
+    "url": "/abigailboyer/starfleetacademy/static/js/main.420c457e.chunk.js"
+  },
+  {
+    "revision": "12716dd344a6d40aaf5c",
+    "url": "/abigailboyer/starfleetacademy/static/js/runtime-main.e3884fbc.js"
+  },
+  {
+    "revision": "b04bc80745fa9ad8a9bf51b8b5433413",
+    "url": "/abigailboyer/starfleetacademy/static/media/starfleet-academy.b04bc807.jpg"
+  },
+  {
+    "revision": "8256022d968244ef8b607fe4c6c08875",
+    "url": "/abigailboyer/starfleetacademy/static/media/starfleetacademy-small.8256022d.jpg"
+  }
+]);
