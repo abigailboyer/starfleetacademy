89404d6e2c119701e8f0aced5c38b7346f7d00eb
abigail <abigailboyer1@gmail.com>
2020-07-29 13:52:46 -0500

    add temporary course name placeholders and styling

--- src/components/Academics.js
+++ src/components/Academics.js
@@ -2,7 +2,45 @@ import React from 'react'
 
 function Academics() {
   return(
-    <h1>Academics</h1>
+    /* command only for now, figure out more later */
+    <section id="academics" className="card container yellowBorder">
+      <section id="cirriculum">
+        <ul>
+          <li>
+            <h4>Core Cirriculum</h4>
+            <ul>
+              <li className="category">
+                Category
+                <p className="smallText">Six credits from:</p>
+                <ul className="list">
+                  <li>Course 101</li>
+                  <li>Course 103</li>
+                </ul>
+              </li>
+              <li className="category">
+                Category
+                <p className="smallText">Six credits from:</p>
+                <ul className="list">
+                  <li>Course 101</li>
+                  <li>Course 103</li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h4>Specialization (Command)</h4>
+          </li>
+          <li>
+            <h4>Minor (Physics)</h4>
+          </li>
+        </ul>
+      </section>
+      <section id="planOfStudy">
+      </section>
+      <section id="completionTracker">
+
+      </section>
+    </section>
   )
 }
 
