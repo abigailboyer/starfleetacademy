77a6150663e27f058e476cc5d9e7664744e46880
abigail <abigailboyer1@gmail.com>
2021-07-07 19:16:59 -0500

    prevent unauthorized use of dashboard components

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,13 +3,32 @@ import Header from '../Header'
 import Nav from '../Nav'
 import Cards from './Cards'
 import NavMenu from './NavMenu'
+import Firebase, {FirebaseContext} from '../../firebase'
 
-function Dashboard() {
-  return(
-    <section>
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    Firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        console.log("signed in");
+      } else {
+        this.props.history.push('/login');
+        console.log("user not logged in");
+      }
+    });
+  }
+
+  render() {
+    return(
+      <section>
       <Cards />
-    </section>
-  )
+      </section>
+    )
+  }
 }
 
 export default Dashboard
